d9b31e4e9d8c42e2aed94b0f04cca4799697a1dd
Horacio Iseka <horacio.chiarella@gmail.com>
2020-10-09 01:29:02 -0300

    path /

--- discography/albums/urls.py
+++ discography/albums/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_discography, name='get_discography'),
+    path('/', views.get_discography, name='get_discography'),
 ]
